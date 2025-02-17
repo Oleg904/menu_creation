9f629e89b00f7e1000ebe39805f70c2caa8e101b
Oleg904 <oleg032orm@gmail.com>
2025-02-17 15:18:31 +0500

    1

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import pandas as pd
 
 
-text = pd.read_excel('tm2025-sm.xlsx', sheet_name='Лист1')
 
-print(text.head())
+def open_type_menu():
+    text = pd.read_excel('tm2025-sm.xlsx', sheet_name='Лист1')
+    # return text.tail(1)
+    # return text.head(1)
+    return text.columns.tolist()
+    # return text.tail(1), text.head(1)
+
+
+print(open_type_menu())
