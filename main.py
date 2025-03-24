a512b3cb1ae5df2a3b85f193a8c086d5695808dc
Oleg904 <oleg032orm@gmail.com>
2025-03-24 12:25:53 +0500

    1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from openpyxl import load_workbook
 from tkinter import *
 from tkinter import ttk
@@ -9,34 +10,19 @@ from tkinter.messagebox import showerror, showwarning, showinfo
 
 home_dir = os.path.expanduser("~")
 
+name_of_inst = ''
+
 if not os.path.exists(f"{home_dir}/Desktop/Менюшки"):
     os.mkdir(f"{home_dir}/Desktop/Менюшки")
-
-wb2 = load_workbook("shablon.xlsx")
-sheet2 = wb2.active
 
 def main_window():
     # открытие файла типового меню
     def open_file():
-        # file_menu = filedialog.askopenfile(filetypes=(("EXCEL", ".xlsx"),))
         global file_menu
-        global  wb
-        global sheet
         file_menu = filedialog.askopenfilename(filetypes=(("EXCEL", ".xlsx"),))
-        wb = load_workbook(file_menu, read_only=True)
-        sheet = wb.active
-        showinfo(title="Информация", message=sheet["A6"].value)
+        menu_processing()
 
-    # def secondary_window():
-    #     window = Tk()
-    #     window.title("Информация")
-    #     window.geometry("450x80")
-    #     label = ttk.Label(text="Просто", font=("Arial", 13))
-    #     label.pack()
-
-    def finish():
-        print(sheet["A6"].value)
-        wb.close()
+    def finish():  # закрытие окна
         root.destroy()  # ручное закрытие окна и всего приложения
 
     # создается главное окно
@@ -52,26 +38,35 @@ def main_window():
     btn = ttk.Button(text="Выбрать", command=open_file)
     btn.pack(side=TOP, pady=30, ipadx=27, ipady=7)
 
-
     root.resizable(False, False)  # запрет изменения размеров окна
 
     root.protocol("WM_DELETE_WINDOW", finish)
 
     root.mainloop()
 
-# print(os.path.exists(f"{home_dir}/Desktop/Меню"))
-# print(os.path.isfile(f"{home_dir}/Desktop/Меню/Пример.xlsx"))
 def menu_processing():
-    try:
+    try:  # проверка на наличие
         if len(os.listdir(f"{home_dir}/Desktop/Менюшки")) == 0:
+            workbook = load_workbook(file_menu, read_only=True)
+            sheet = workbook.active
+            workbook2 = load_workbook("shablon.xlsx")
+            sheet2 = workbook2.active
             sheet2[f"C{10}"] = "Пример"
-            wb2.save(f"{home_dir}/Desktop/Менюшки/Пример.xlsx")
+            workbook2.save(f"{home_dir}/Desktop/Менюшки/Пример.xlsx")
+            showinfo(title="Информация", message="Файлы меню созданы на Рабочем столе в папке Менюшки. Программу можно закрыть.")
+            workbook.close()
+
         else:
             showinfo(title="Информация", message="В папке содержатся старые файлы меню. Эти файлы будут перезаписаны.")
+            shutil.rmtree(f"{home_dir}/Desktop/Менюшки")
+            os.mkdir(f"{home_dir}/Desktop/Менюшки")
+            menu_processing()
 
-    except BaseException as e:
-        print(e)
-    else:
-        showinfo(title="Информация", message="Файлы меню созданы на Рабочем столе в папке Менюшки")
+    except BaseException as errors:
+        if 'WinError 32' in str(errors):
+            showinfo(title="Информация", message='Закройте файл меню и заново выберите файл типового меню.')
+        else:
+            showinfo(title="Информация", message=errors)
+
 
 main_window()
